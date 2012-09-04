a0123668b8f442a3b7b63501df7c169124b07aca
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-09-04 19:27:15 +0900

    2012-09-05

---
+++ 2012-09-05/1.scm
@@ -0,0 +1,4 @@
+;1-1
+(define a (- (* 4 2) (/ 21 3)))
+(define b (- (expt 3 3) 4))
+(define c (and (= a 1) (> b 0)))
---
+++ 2012-09-05/2.scm
@@ -0,0 +1,4 @@
+;1-2
+(define (volume x) (/ (expt x 3) (* 6 (sqrt 2))))
+(define (clog x) (/ (log x) (log 10)))
+(define (f x) (- (+ (expt x 3) (* 4 (expt x 2))) (* 8 x) 3))
