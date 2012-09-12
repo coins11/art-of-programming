e680466e20c1c37e016843d4e6a836cda57549bc
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-09-12 11:43:23 +0900

    2012-09-12

--- 2012-09-12/1.scm
+++ 2012-09-12/1.scm
@@ -1,2 +1,15 @@
 ;2-1
-(define (abs1 x) (if (< x 0) (- x) x))
+(define (middle3 x y z)
+  (cond
+    ((and (<= y x) (<= x z)) x)
+    ((and (<= z x) (<= x y)) x)
+    ((and (<= x y) (<= y z)) y)
+    ((and (<= z y) (<= y x)) y)
+    ((and (<= x z) (<= z y)) z)
+    ((and (<= y z) (<= z x)) z)))
+
+(define (fold-left kons knil l)
+  (if (null? l) knil (fold-left kons (kons (car l) knil) (cdr l))))
+(define (max x y) (if (<= x y) y x))
+(define (maximum x . l) (fold-left max x l))
+(define max5 maximum)
--- 2012-09-12/3.scm
+++ 2012-09-12/3.scm
@@ -1,12 +1,12 @@
-;3-2
+;2-3
 (define pi 3.141592653589793)
 (define (area kind a b)
   (cond ((and (eq? kind 'TRIANGLE) (< 0 a) (< 0 b))
             (/ (* a b) 2))
         ((and (eq? kind 'RECTANGLE) (< 0 a) (< 0 b))
             (* a b))
-        ((and (eq? kind 'ELLIPSE) (< 0 a) (< 0 b))
-            (* pi a b))
+        ((and (eq? kind 'CYLINDER) (< 0 a) (< 0 b))
+            (* (+ (* pi a a) (* pi a b)) 2))
         ((and (eq? kind 'SECTOR) (< 0 a) (< 0 b) (<= b 360))
             (* pi a a (/ b 360)))
         (else
